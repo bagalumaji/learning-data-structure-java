ba2c389c32fdd6d0bb1c39ac73526be31034bc0d
bagalumaji <bagalumaji@gmail.com>
2023-11-24 10:27:30 -0800

    binary search - collection

---
+++ src/main/java/com/bagal/searching/BinarySearch.java
@@ -0,0 +1,21 @@
+package com.bagal.searching;
+
+public class BinarySearch {
+    public static int searchElementByUsingBinarySearch(int [] arr,int value){
+        int index=-1;
+        int begin=0,end=arr.length-1,mid;
+        while(begin<=end){
+            mid = (begin+end)/2;
+            if(arr[mid] == value){
+                index=mid;
+                break;
+            }
+            if(value>arr[mid]){
+                begin=mid+1;
+            }else {
+                end=mid-1;
+            }
+        }
+        return index;
+    }
+}
