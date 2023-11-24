508c31c6be4b2450eb804e8a70448d81353cb455
bagalumaji <bagalumaji@gmail.com>
2023-11-23 22:45:21 -0800

    test

--- src/main/java/com/bagal/Main.java
+++ src/main/java/com/bagal/Main.java
@@ -1,7 +1,13 @@
 package com.bagal;
 
+import com.bagal.sorting.SelectionSortCollection;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> list = Arrays.asList(2,1,5,4,3);
+        System.out.println(SelectionSortCollection.sortValuesByUsingSelectionSort(list));
     }
 }
