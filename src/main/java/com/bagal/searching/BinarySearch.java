17291ce587c1d7e319f77208f88a9d530f467890
bagalumaji <bagalumaji@gmail.com>
2023-11-27 18:36:40 -0800

    binary search - array

--- src/main/java/com/bagal/searching/BinarySearch.java
+++ src/main/java/com/bagal/searching/BinarySearch.java
@@ -1,5 +1,6 @@
 package com.bagal.searching;
 
+import java.util.List;
 
 public class BinarySearch {
     public static int searchElementByUsingBinarySearch(int [] arr,int value){
@@ -12,6 +13,23 @@ public class BinarySearch {
                 break;
             }
             if(value>arr[mid]){
+                begin=mid+1;
+            }else {
+                end=mid-1;
+            }
+        }
+        return index;
+    }
+    public static int searchElementByUsingBinarySearch(List<Integer> arr, int value){
+        int index=-1;
+        int begin=0,end=arr.size()-1,mid;
+        while(begin<=end){
+            mid = (begin+end)/2;
+            if(arr.get(mid) == value){
+                index=mid;
+                break;
+            }
+            if(value>arr.get(mid)){
                 begin=mid+1;
             }else {
                 end=mid-1;
